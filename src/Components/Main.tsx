b9344582163f8b5c45b468a95b40b8d0afddfa1d
Laurence Qi <laurence.qi@outlook.com>
2022-04-18 19:58:20 +1000

    successfully able to upload files

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,5 +1,6 @@
 
 import React, {useState} from "react";
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
 
 interface File {
     lastModified: number;
@@ -11,30 +12,46 @@ interface File {
 
 
 export const Main = () => {
+    const [numPages, setNumPages] = useState<number | null> (null);
+    const [pageNumber, setPageNumber] = useState<number> (1);
 
-    const [hello, setHello] = useState('');
-    const [file, setFile] = React.useState<File | null>();
+    const [file, setFile] = React.useState<any | null>();
     const fileReader = new FileReader();
 
     fileReader.addEventListener("load", () => {
         // this will then display a text file
-        console.log(fileReader.result);
+        // debugger;
+
+        const base64String = fileReader.result
+        setFile(base64String);
+        console.log(fileReader.result)
 
     }, false);
 
-    // upload the file
-    const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files){
+    function onDocumentLoadSuccess({ numPages }: {numPages: number}) {
+        setNumPages(numPages);
+    }
 
-            fileReader.readAsDataURL(event.target.files[0]);
-        }
-    };
+    // upload the file
+    const onFileChange = (event: any) => {
+        setFile(event.target.files[0]);
+      }
 
     return <div>
         <input type="file" onChange={onFileChange}/>
         
         <h1>Upload your image here </h1>
-        <p>{file?.name}</p>
+        {/* <p>{file?.name}</p> */}
+
+        <div>
+            <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
+            </Document>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
+        </div>
+        
     </div>;
 }
 
