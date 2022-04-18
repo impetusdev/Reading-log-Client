e13e6620b021ecb9a2584a7a69251ce4950c86ad
Laurence Qi <laurence.qi@outlook.com>
2022-04-19 08:14:30 +1000

    added a page incrementer

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -15,7 +15,7 @@ export const Main = () => {
     const [numPages, setNumPages] = useState<number | null> (null);
     const [pageNumber, setPageNumber] = useState<number> (1);
 
-    const [file, setFile] = React.useState<any | null>();
+    const [file, setFile] = React.useState<any | null>(null);
     const fileReader = new FileReader();
 
     fileReader.addEventListener("load", () => {
@@ -35,23 +35,35 @@ export const Main = () => {
     // upload the file
     const onFileChange = (event: any) => {
         setFile(event.target.files[0]);
-      }
+    }
+
+    const incrementPageNumber = () => {
+        setPageNumber(pageNumber + 1);
+    }
+    const decrementPageNumber = () => {
+        if (pageNumber !== 1) {
+            setPageNumber(pageNumber - 1);
+        }
+    }
 
     return <div>
+        <h1>Speed Reader</h1>
+        <p>Upload your file below and begin</p>
         <input type="file" onChange={onFileChange}/>
-        
-        <h1>Upload your image here </h1>
-        {/* <p>{file?.name}</p> */}
-
-        <div>
-            <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
-                <Page pageNumber={pageNumber} />
-            </Document>
-            <p>
-                Page {pageNumber} of {numPages}
-            </p>
-        </div>
-        
+        <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+        </Document>
+        {file ?
+            <div>
+                <p>
+                    Page {pageNumber} of {numPages}
+                </p>
+                <button onClick={decrementPageNumber}>Prev</button>
+                <button onClick={incrementPageNumber}>Next</button>
+            </div>
+            :
+            ''
+        }
     </div>;
 }
 
