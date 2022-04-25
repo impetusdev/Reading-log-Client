ed98465e99fc8fe98445a4e42a80a3304880bfa1
Laurence Qi <laurence.qi@outlook.com>
2022-04-25 22:25:34 +1000

    Added CORS BUT BUG

--- src/Components/Text.tsx
+++ src/Components/Text.tsx
@@ -1,5 +1,6 @@
 
 import React, {useState} from "react";
+import axios from "axios";
 import './Text.scss';
 
 export const Text = () => {
@@ -12,6 +13,15 @@ export const Text = () => {
     
     // TODO: use this website: https://www.smashingmagazine.com/2020/07/react-apps-testing-library/
     // count how many words are in the text. 
+    const getText = async () => { 
+        try {
+            let res = await axios.get('localhost:3000/text')
+            console.log('res is:', res);
+        } catch (error) {
+            console.log("Error:", error)
+        }    
+    }
+
     const analyseText = () => { 
         if (textSnippet.trim() === ''){
             setWordCount(0);
@@ -24,6 +34,7 @@ export const Text = () => {
         <h2>Input your text below</h2>
         <textarea onChange={updateText}/>
         <p>{textSnippet}</p>
+        <button onClick={getText}>Upload the text file</button>
         <button onClick={analyseText}>Analyse</button>
         <p>{`word count: ${wordCount}`}</p>
     </div>;
