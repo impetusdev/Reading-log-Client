8f9a40f4887b6fa7efe60914d2492cd0611b9f78
Laurence Qi <laurence.qi@outlook.com>
2022-05-09 08:44:47 +1000

    added a WPM calculator

--- src/Components/ActivePassage.tsx
+++ src/Components/ActivePassage.tsx
@@ -6,8 +6,9 @@ import {Passage} from './Read';
 export const ActivePassage = ({passage}: any) => {
     
     const [timerStarted, setTimerStarted] = useState<boolean>(false);
-    const [time, setTime] = useState<number>(0);
     const [timerIntervalId, setTimerIntervalId] = useState<any | null>(null); //TODO: get the type correct on this. 
+    const [time, setTime] = useState<number>(0);
+    const [wpm, setWpm] = useState<number>(0);
     
     const startTimer = () => {
         setTimerStarted(true);
@@ -20,26 +21,30 @@ export const ActivePassage = ({passage}: any) => {
         setTimerStarted(false);
         console.log('intervalId:', timerIntervalId)
         clearInterval(timerIntervalId);
+        
     }
 
-    console.log('passage:', passage);
+    const submit = () => {
+        setWpm(60 * passage.wordCount/ time);
+        //TODO: put in the submission times. 
+    }
   
     return (
         passage ? <div className="passage">
             <h2>{passage.source}</h2>
             <h3>{passage.author}</h3>
-            <p>{passage.text}</p>
-            <p>{time.toFixed(1)}</p>
+            <p className="text">{passage.text}</p>
+            <p>{wpm} Words per Minute</p>
             <p className="difficulty">Difficulty: {passage.complexity}</p>
             
+            <p>{time.toFixed(1)}</p>
             {
                 timerStarted ? 
-                    <button onClick={stopTimer}>Finished</button> 
+                    <button onClick={stopTimer}>Pause</button> 
                     :
                     <button onClick={startTimer}>Start Reading</button>
             }
-            
-            <button>Submit Time</button>
+            <button onClick={submit} disabled={time === 0 ? true : false}>Submit Time</button>
         </div>
         : <></>
     )
