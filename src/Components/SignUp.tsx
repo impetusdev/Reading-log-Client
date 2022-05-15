b69ba8d92ee9aea0787f06efd3a0aaa504adbadb
Laurence Qi <laurence.qi@outlook.com>
2022-05-15 12:05:25 +1000

    added signup logic

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -5,16 +5,28 @@ import './Signup.scss';
 
 
 export const SignUp = (): JSX.Element => {
-    
-    
-    // TODO: implement a react form that performs the axios calls to login etc. 
+    const [email, setEmail] = useState<string>('laurence.qi@outlook.com');
+    const [password, setPassword] = useState<string>('chicken');
+
+    const handleSubmit = async (event: React.SyntheticEvent) => {
+        // performs the axios request
+        event.preventDefault();
+        const response = await axios.post('http://localhost:3000/login', {
+            email,
+            password
+        });
+
+        console.log(response) //TODO: set this token in the auth context. 
+    }
+
     return (
         <div className="container">
-            <form>
+            <form onSubmit={handleSubmit}>
                     <label>Email</label>
-                    <input type="text" />
+                    <input type="text" onChange={(e) => setEmail(e.target.value)} value="laurence.qi@outlook.com"/>
                     <label>Password</label>
-                    <input type="password" />
+                    <input type="password" onChange={(e) => setPassword(e.target.value)} value="chicken"/>
+                    <input type="submit" />
             </form>
         </div>
     )
